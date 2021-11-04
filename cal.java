0ff92ad30ab519578026f8800c6e3d1c08ac1ee4
Cian Crowley-Smith <ciano4fr@gmail.com>
2021-11-04 13:54:40 +0000

    added assivgmnet1 branch

--- cal.java
+++ cal.java
@@ -4,42 +4,41 @@ import org.antlr.v4.runtime.CharStreams;
 import java.io.FileInputStream;
 import org.antlr.v4.runtime.DefaultErrorStrategy;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
-import java.io.*; 
+import java.io.*;
 
-public class cal
-{
-	public static void main (String[] args) throws Exception
-	{
+public class cal {
+	public static void main(String[] args) throws Exception {
 		String inputFile = null;
 
 		if (args.length > 0)
-			inputFile = args [0];
+			inputFile = args[0];
 
-	    InputStream is = System.in;
-	   	if (inputFile!=null) 
-	   		is = new FileInputStream(inputFile);
+		InputStream is = System.in;
+		if (inputFile != null)
+			is = new FileInputStream(inputFile);
 
-	    calLexer lexer = new calLexer (CharStreams.fromStream(is));
-		CommonTokenStream tokens = new CommonTokenStream (lexer);
-		calParser parser = new calParser (tokens);
+		calLexer lexer = new calLexer(CharStreams.fromStream(is));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		calParser parser = new calParser(tokens);
 
-		parser.setErrorHandler(new DefaultErrorStrategy(){
+		parser.setErrorHandler(new DefaultErrorStrategy() {
 
 			@Override
 			public void recover(Parser recognizer, RecognitionException e) {
-				for (ParserRuleContext context = recognizer.getContext(); context != null; context = context.getParent()) {
+				for (ParserRuleContext context = recognizer.getContext(); context != null; context = context
+						.getParent()) {
 					context.exception = e;
 				}
 
 				throw new ParseCancellationException(e);
-				
+
 			}
 
 			@Override
-			public Token recoverInline(Parser recognizer)
-			{
+			public Token recoverInline(Parser recognizer) {
 				InputMismatchException e = new InputMismatchException(recognizer);
-				for (ParserRuleContext context = recognizer.getContext(); context != null; context = context.getParent()) {
+				for (ParserRuleContext context = recognizer.getContext(); context != null; context = context
+						.getParent()) {
 					context.exception = e;
 				}
 
@@ -52,10 +51,9 @@ public class cal
 			System.out.println(args[0] + " parsed successfully");
 		}
 
-		catch(ParseCancellationException e){
+		catch (ParseCancellationException e) {
 			System.out.println(args[0] + " has not parsed");
 		}
-
 
 	}
 
